380dcafd14a6dfcca6d3dec7dd8bbbe06b225bdb
clipflip-rblx <gogetter3english1@gmail.com>
2024-06-16 18:26:58 +0300

    Update latest-loader.lua

--- latest-loader.lua
+++ latest-loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/clipflip-rblx/clipflip-gui/master/beta'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/clipflip-rblx/clipflip-gui/master/2-0-32.lua'))()
